ca8f241129bafa7dabe58f72d6ab61e7c05d062f
Anas Khan <annaasskhan6@gmail.com>
2025-04-07 14:30:54 +0530

    Fix mobile layout issue on favourites page and style updates

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -5,25 +5,24 @@ import MovieCard from "../components/MovieCard";
 function Favourites() {
   const { favourites } = useMovieContext();
 
-  if (favourites) {
-    return (
-      <div className="favourites">
-        <h2>Your Favourites</h2>
+  return (
+    <div className="favourites">
+      <h2>Your Favourites</h2>
+
+      {favourites.length === 0 ? (
+        <div className="favourites-empty">
+          <h2>No Favourite Movies Yet</h2>
+          
+        </div>
+      ) : (
         <div className="movies-grid">
           {favourites.map((movie) => (
-            <MovieCard movie={movie} key={movie.id} />
+            <MovieCard movie={movie} key={movie.imdbID} />
+            
           ))}
         </div>
-      </div>
-    );
-  }
-
-  return (
-    <div className="favourites-empty">
-      <h2>No Favourite Movies Yet</h2>
-      <p>Start adding movies to your favourites and they will appear here!</p>
+      )}
     </div>
   );
 }
-
 export default Favourites;
